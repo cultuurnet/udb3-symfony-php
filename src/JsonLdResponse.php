b85ef2cfb265f162984bf0a8537e57dd06a50b7e
Kristof Coomans <kristof@coomans.eu>
2014-10-23 13:14:04 +0200

    Add JsonLdResponse class

---
+++ src/JsonLdResponse.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\Symfony;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class JsonLdResponse extends JsonResponse
+{
+    public function __construct($data = null, $status = 200, $headers = array())
+    {
+        $headers += array(
+          'Content-Type' => 'application/ld+json',
+        );
+
+        parent::__construct(
+            $data,
+            $status,
+            $headers
+        );
+    }
+} 
