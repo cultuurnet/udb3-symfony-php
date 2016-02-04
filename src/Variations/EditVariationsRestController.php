bf319ffb92caf5a82caf9bc227f3621eb89a42b9
Bert Ramakers <bertramakers@me.com>
2016-02-04 12:00:28 +0100

    III-703: Variations controllers.

---
+++ src/Variations/EditVariationsRestController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace CultuurNet\UDB3\Symfony\Variations;
+
+use Broadway\CommandHandling\CommandBusInterface;
+use CultuurNet\UDB3\Event\ReadModel\DocumentGoneException;
+use CultuurNet\UDB3\Event\ReadModel\DocumentRepositoryInterface;
+use CultuurNet\UDB3\Symfony\CommandDeserializerControllerTrait;
+use CultuurNet\UDB3\Variations\Command\DeleteEventVariation;
+use CultuurNet\UDB3\Variations\Command\EditDescriptionJSONDeserializer;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\GoneHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class EditVariationsRestController
+{
+    use CommandDeserializerControllerTrait;
+
+    /**
+     * @var DocumentRepositoryInterface
+     */
+    private $repository;
+
+    /**
+     * @param DocumentRepositoryInterface $repository
+     * @param CommandBusInterface $commandBus
+     */
+    public function __construct(
+        DocumentRepositoryInterface $repository,
+        CommandBusInterface $commandBus
+    ) {
+        $this->repository = $repository;
+        $this->setCommandBus($commandBus);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     */
+    public function edit(Request $request, $id)
+    {
+        $this->guardId($id);
+
+        $deserializer = new EditDescriptionJSONDeserializer($id);
+
+        return $this->handleRequestWithDeserializer(
+            $request,
+            $deserializer
+        );
+    }
+
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
+    public function delete($id)
+    {
+        $this->guardId($id);
+
+        $command = new DeleteEventVariation($id);
+
+        return $this->handleCommand($command);
+    }
+
+    /**
+     * @param string $id
+     */
+    private function guardId($id)
+    {
+        try {
+            $document = $this->repository->get($id);
+        }
+        catch (DocumentGoneException $e) {
+            throw new GoneHttpException();
+        }
+
+        if (!$document) {
+            throw new NotFoundHttpException();
+        }
+    }
+}
