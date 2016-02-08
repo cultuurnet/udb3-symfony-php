5f4314e4c46de297cdd8871433444259552c01ec
Nick <nick@2dotstwice.be>
2016-02-08 15:00:56 +0100

    III-700: OrganizerController:createOrganizer

--- src/Organizer/OrganizerController.php
+++ src/Organizer/OrganizerController.php
@@ -3,9 +3,15 @@
 namespace CultuurNet\UDB3\Symfony\Organizer;
 
 use CultuurNet\Hydra\PagedCollection;
+use CultuurNet\UDB3\Address;
 use CultuurNet\UDB3\EntityServiceInterface;
+use CultuurNet\UDB3\Iri\IriGeneratorInterface;
+use CultuurNet\UDB3\Organizer\OrganizerEditingServiceInterface;
 use CultuurNet\UDB3\Organizer\ReadModel\Lookup\OrganizerLookupServiceInterface;
 use CultuurNet\UDB3\Symfony\JsonLdResponse;
+use CultuurNet\UDB3\Title;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class OrganizerController
 {
@@ -19,15 +25,25 @@ class OrganizerController
      */
     private $lookupService;
 
+    /** @var OrganizerEditingServiceInterface */
+    private $editingService;
+
+    /** @var IriGeneratorInterface */
+    private $iriGenerator;
+
     /**
      * @param EntityServiceInterface $service
      */
     public function __construct(
         EntityServiceInterface $service,
-        OrganizerLookupServiceInterface $organizerLookupService
+        OrganizerLookupServiceInterface $organizerLookupService,
+        OrganizerEditingServiceInterface $organizerEditingService,
+        IriGeneratorInterface $organizerIriGenerator
     ) {
         $this->service = $service;
         $this->lookupService = $organizerLookupService;
+        $this->editingService = $organizerEditingService;
+        $this->iriGenerator = $organizerIriGenerator;
     }
 
     /**
@@ -80,5 +96,69 @@ class OrganizerController
         );
 
         return (new JsonLdResponse($pagedCollection));
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function createOrganizer(Request $request)
+    {
+        $response = new JsonResponse();
+        $body_content = json_decode($request->getContent());
+
+        try {
+            if (empty($body_content->name)) {
+                throw new \InvalidArgumentException('Required fields are missing');
+            }
+
+            $addresses = array();
+            if (!empty($body_content->address->streetAddress) &&
+                !empty($body_content->address->locality) &&
+                !empty($body_content->address->postalCode) &&
+                !empty($body_content->address->country)) {
+                $addresses[] = new Address(
+                    $body_content->address->streetAddress,
+                    $body_content->address->postalCode,
+                    $body_content->address->locality,
+                    $body_content->address->country
+                );
+            }
+
+            $phones = array();
+            $emails = array();
+            $urls = array();
+            if (!empty($body_content->contact)) {
+                foreach ($body_content->contact as $contactInfo) {
+                    if ($contactInfo->type == 'phone') {
+                        $phones[] = $contactInfo->value;
+                    } elseif ($contactInfo->type == 'email') {
+                        $emails[] = $contactInfo->value;
+                    } elseif ($contactInfo->type == 'url') {
+                        $urls[] = $contactInfo->value;
+                    }
+                }
+            }
+
+            $organizer_id = $this->editingService->createOrganizer(
+                new Title($body_content->name),
+                $addresses,
+                $phones,
+                $emails,
+                $urls
+            );
+
+            $response->setData(
+                [
+                    'organizerId' => $organizer_id,
+                    'url' => $this->iriGenerator->iri($organizer_id),
+                ]
+            );
+        } catch (\Exception $e) {
+            $response->setStatusCode(400);
+            $response->setData(['error' => $e->getMessage()]);
+        }
+
+        return $response;
     }
 }
