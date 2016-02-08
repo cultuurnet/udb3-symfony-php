42b4f4774f5a2a51dec4a785cf869de89c1a58c3
Nick <nick@2dotstwice.be>
2016-02-08 14:30:08 +0100

    III-700: OrganizerController:findByPartOfTitle

--- src/Organizer/OrganizerController.php
+++ src/Organizer/OrganizerController.php
@@ -2,7 +2,9 @@
 
 namespace CultuurNet\UDB3\Symfony\Organizer;
 
+use CultuurNet\Hydra\PagedCollection;
 use CultuurNet\UDB3\EntityServiceInterface;
+use CultuurNet\UDB3\Organizer\ReadModel\Lookup\OrganizerLookupServiceInterface;
 use CultuurNet\UDB3\Symfony\JsonLdResponse;
 
 class OrganizerController
@@ -13,12 +15,19 @@ class OrganizerController
     private $service;
 
     /**
+     * @var OrganizerLookupServiceInterface
+     */
+    private $lookupService;
+
+    /**
      * @param EntityServiceInterface $service
      */
     public function __construct(
-        EntityServiceInterface $service
+        EntityServiceInterface $service,
+        OrganizerLookupServiceInterface $organizerLookupService
     ) {
         $this->service = $service;
+        $this->lookupService = $organizerLookupService;
     }
 
     /**
@@ -39,5 +48,33 @@ class OrganizerController
         $response->headers->set('Vary', 'Origin');
 
         return $response;
+    }
+
+    function findByPartOfTitle($term)
+    {
+        // @todo Add & process pagination parameters
+
+        $ids = $this->lookupService->findOrganizersByPartOfTitle($term);
+
+        $members = [];
+        if (!empty($ids)) {
+            $organizerService = $this->service;
+
+            $members = array_map(
+                function ($id) use ($organizerService) {
+                    return json_decode($organizerService->getEntity($id));
+                },
+                $ids
+            );
+        }
+
+        $pagedCollection = new PagedCollection(
+            1,
+            1000,
+            $members,
+            count($members)
+        );
+
+        return (new JsonLdResponse($pagedCollection));
     }
 }
