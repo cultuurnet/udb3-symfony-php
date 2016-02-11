0053501466d7f50f2e339b293605f0aab1b6b1b4
Bert Ramakers <bertramakers@me.com>
2016-02-11 12:52:29 +0100

    III-697: Removed label memory logic from EditOfferRestController.

--- src/Offer/EditOfferRestController.php
+++ src/Offer/EditOfferRestController.php
@@ -5,7 +5,6 @@ namespace CultuurNet\UDB3\Symfony\Offer;
 use CultuurNet\Deserializer\DeserializerInterface;
 use CultuurNet\UDB3\Label;
 use CultuurNet\UDB3\Offer\OfferEditingServiceInterface;
-use CultuurNet\UDB3\UsedLabelsMemory\UsedLabelsMemoryServiceInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use ValueObjects\String\String;
@@ -18,16 +17,6 @@ class EditOfferRestController
     private $editService;
 
     /**
-     * @var \CultureFeed_User
-     */
-    private $user;
-
-    /**
-     * @var UsedLabelsMemoryServiceInterface
-     */
-    private $labelMemory;
-
-    /**
      * @var DeserializerInterface
      */
     private $labelJsonDeserializer;
@@ -35,19 +24,13 @@ class EditOfferRestController
     /**
      * EditOfferRestController constructor.
      * @param OfferEditingServiceInterface $editingServiceInterface
-     * @param \CultureFeed_User $user
-     * @param UsedLabelsMemoryServiceInterface $labelMemory
      * @param DeserializerInterface $labelJsonDeserializer
      */
     public function __construct(
         OfferEditingServiceInterface $editingServiceInterface,
-        \CultureFeed_User $user,
-        UsedLabelsMemoryServiceInterface $labelMemory,
         DeserializerInterface $labelJsonDeserializer
     ) {
         $this->editService = $editingServiceInterface;
-        $this->user = $user;
-        $this->labelMemory = $labelMemory;
         $this->labelJsonDeserializer = $labelJsonDeserializer;
     }
 
@@ -63,11 +46,6 @@ class EditOfferRestController
 
         $commandId = $this->editService->addLabel(
             $cdbid,
-            $label
-        );
-
-        $this->labelMemory->rememberLabelUsed(
-            $this->user->id,
             $label
         );
 
