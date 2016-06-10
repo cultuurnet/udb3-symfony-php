194e3a4360a19dc0838389fe6fe3756b26332f9f
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-06-10 15:26:20 +0200

    III-1092: Adapt ReadORganizerRestController & add tests

--- src/Organizer/ReadOrganizerRestController.php
+++ src/Organizer/ReadOrganizerRestController.php
@@ -4,11 +4,18 @@ namespace CultuurNet\UDB3\Symfony\Organizer;
 
 use CultuurNet\Hydra\PagedCollection;
 use CultuurNet\UDB3\EntityServiceInterface;
+use CultuurNet\UDB3\Event\ReadModel\DocumentGoneException;
 use CultuurNet\UDB3\Organizer\ReadModel\Lookup\OrganizerLookupServiceInterface;
+use CultuurNet\UDB3\Symfony\ApiProblemJsonResponseTrait;
 use CultuurNet\UDB3\Symfony\JsonLdResponse;
 
 class ReadOrganizerRestController
 {
+    const GET_ERROR_NOT_FOUND = 'An error occurred while getting the event with id %s!';
+    const GET_ERROR_GONE = 'An error occurred while getting the event with id %s which was removed!';
+
+    use ApiProblemJsonResponseTrait;
+
     /**
      * @var EntityServiceInterface
      */
@@ -39,15 +46,25 @@ class ReadOrganizerRestController
      */
     public function get($cdbid)
     {
-        $organizer = $this->service->getEntity($cdbid);
+        $response = null;
+        
+        try {
+            $organizer = $this->service->getEntity($cdbid);
 
-        $response = JsonLdResponse::create()
-            ->setContent($organizer)
-            ->setPublic()
-            ->setClientTtl(60 * 30)
-            ->setTtl(60 * 5);
+            if ($organizer) {
+                $response = JsonLdResponse::create()
+                    ->setContent($organizer)
+                    ->setPublic()
+                    ->setClientTtl(60 * 30)
+                    ->setTtl(60 * 5);
 
-        $response->headers->set('Vary', 'Origin');
+                $response->headers->set('Vary', 'Origin');
+            } else {
+                $response = $this->createApiProblemJsonResponseNotFound(self::GET_ERROR_NOT_FOUND, $cdbid);
+            }
+        } catch (DocumentGoneException $documentGoneException) {
+            $response = $this->createApiProblemJsonResponseGone(self::GET_ERROR_GONE, $cdbid);
+        }
 
         return $response;
     }
