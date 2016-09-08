25bacb3dcb2acd5ee3478ad888e4bc00068e7005
Luc Wollants <luc.wollants@apptite.be>
2016-09-08 09:21:54 +0200

    III-1379 Added basic read rest controller for jobs.

---
+++ src/Jobs/ReadRestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Cultuurnet\UDB3\Symfony\Jobs;
+
+use Resque_Job_Status;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ReadRestController
+{
+    public function get($jobId)
+    {
+        $status = new Resque_Job_Status($jobId);
+        return new JsonResponse($status);
+    }
+}
