ddb514684a836ba88b5967c2acce96e1ce2da6eb
Luc Wollants <luc.wollants@apptite.be>
2016-09-08 12:36:56 +0200

    III-1379 The read rest controller for job status now uses a factory to create the job status and return an api problem in case of error.

--- src/Jobs/ReadRestController.php
+++ src/Jobs/ReadRestController.php
@@ -2,14 +2,45 @@
 
 namespace CultuurNet\UDB3\Symfony\Jobs;
 
-use Resque_Job_Status;
+use Crell\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Symfony\HttpFoundation\ApiProblemJsonResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use ValueObjects\String\String as StringLiteral;
 
 class ReadRestController
 {
+    /**
+     * @var JobsStatusFactoryInterface
+     */
+    private $jobStatusFactory;
+
+    /**
+     * ReadRestController constructor.
+     * @param JobsStatusFactoryInterface $jobStatusFactory
+     */
+    public function __construct(JobsStatusFactoryInterface $jobStatusFactory)
+    {
+        $this->jobStatusFactory = $jobStatusFactory;
+    }
+
+    /**
+     * @param $jobId
+     * @return ApiProblemJsonResponse|JsonResponse
+     */
     public function get($jobId)
     {
-        $status = new Resque_Job_Status($jobId);
-        return new JsonResponse($status);
+        $jobStatus = $this->jobStatusFactory->createFromJobId(
+            new StringLiteral($jobId)
+        );
+
+        if ($jobStatus) {
+            return new JsonResponse($jobStatus->toNative());
+        } else {
+            $apiProblem = new ApiProblem('No status for job with id: ' . $jobId);
+            $apiProblem->setStatus(Response::HTTP_BAD_REQUEST);
+
+            return new ApiProblemJsonResponse($apiProblem);
+        }
     }
 }
