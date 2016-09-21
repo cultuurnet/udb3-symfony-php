613992cc6d6ea186473b95fb995c2f1b5d8ea4b5
Bert Ramakers <bertramakers@me.com>
2016-09-21 11:13:07 +0200

    III-1325: Use EventTypeJSONDeserializer and ThemeJSONDeserializer in MajorInfoJSONDeserializers.

--- src/Deserializer/Event/MajorInfoJSONDeserializer.php
+++ src/Deserializer/Event/MajorInfoJSONDeserializer.php
@@ -7,6 +7,7 @@ use CultuurNet\Deserializer\JSONDeserializer;
 use CultuurNet\UDB3\Event\EventType;
 use CultuurNet\UDB3\Symfony\Deserializer\CalendarDeserializer;
 use CultuurNet\UDB3\Symfony\Deserializer\Location\LocationJSONDeserializer;
+use CultuurNet\UDB3\Symfony\Deserializer\Theme\ThemeJSONDeserializer;
 use CultuurNet\UDB3\Theme;
 use CultuurNet\UDB3\Title;
 use ValueObjects\String\String as StringLiteral;
@@ -24,9 +25,19 @@ class MajorInfoJSONDeserializer extends JSONDeserializer
     private $locationDeserializer;
 
     /**
+     * @var EventTypeJSONDeserializer
+     */
+    private $typeDeserializer;
+
+    /**
      * @var CalendarDeserializer
      */
     private $calendarDeserializer;
+
+    /**
+     * @var ThemeJSONDeserializer
+     */
+    private $themeDeserializer;
 
     public function __construct()
     {
@@ -34,8 +45,11 @@ class MajorInfoJSONDeserializer extends JSONDeserializer
         parent::__construct($assoc);
 
         $this->validator = new MajorInfoDataValidator();
+
+        $this->typeDeserializer = new EventTypeJSONDeserializer();
         $this->locationDeserializer = new LocationJSONDeserializer();
         $this->calendarDeserializer = new CalendarDeserializer();
+        $this->themeDeserializer = new ThemeJSONDeserializer();
     }
 
     /**
@@ -48,6 +62,10 @@ class MajorInfoJSONDeserializer extends JSONDeserializer
         $data = parent::deserialize($data);
         $this->validator->validate($data);
 
+        $type = $this->typeDeserializer->deserialize(
+            new StringLiteral(json_encode($data['type']))
+        );
+
         $location = $this->locationDeserializer->deserialize(
             new StringLiteral(json_encode($data['location']))
         );
@@ -56,12 +74,14 @@ class MajorInfoJSONDeserializer extends JSONDeserializer
 
         $theme = null;
         if (!empty($data['theme'])) {
-            $theme = new Theme($data['theme']['id'], $data['theme']['label']);
+            $theme = $this->themeDeserializer->deserialize(
+                new StringLiteral(json_encode($data['theme']))
+            );
         }
 
         return new MajorInfo(
             new Title($data['name']['nl']),
-            new EventType($data['type']['id'], $data['type']['label']),
+            $type,
             $location,
             $calendar,
             $theme
--- src/Deserializer/Place/MajorInfoJSONDeserializer.php
+++ src/Deserializer/Place/MajorInfoJSONDeserializer.php
@@ -4,9 +4,10 @@ namespace CultuurNet\UDB3\Symfony\Deserializer\Place;
 
 use CultuurNet\Deserializer\DataValidationException;
 use CultuurNet\Deserializer\JSONDeserializer;
-use CultuurNet\UDB3\Event\EventType;
 use CultuurNet\UDB3\Symfony\Deserializer\Address\AddressJSONDeserializer;
 use CultuurNet\UDB3\Symfony\Deserializer\CalendarDeserializer;
+use CultuurNet\UDB3\Symfony\Deserializer\Event\EventTypeJSONDeserializer;
+use CultuurNet\UDB3\Symfony\Deserializer\Theme\ThemeJSONDeserializer;
 use CultuurNet\UDB3\Theme;
 use CultuurNet\UDB3\Title;
 use ValueObjects\String\String as StringLiteral;
@@ -19,6 +20,11 @@ class MajorInfoJSONDeserializer extends JSONDeserializer
     private $validator;
 
     /**
+     * @var EventTypeJSONDeserializer
+     */
+    private $typeDeserializer;
+
+    /**
      * @var AddressJSONDeserializer
      */
     private $addressDeserializer;
@@ -28,14 +34,22 @@ class MajorInfoJSONDeserializer extends JSONDeserializer
      */
     private $calendarDeserializer;
 
+    /**
+     * @var ThemeJSONDeserializer
+     */
+    private $themeDeserializer;
+
     public function __construct()
     {
         $assoc = true;
         parent::__construct($assoc);
 
         $this->validator = new MajorInfoDataValidator();
+
+        $this->typeDeserializer = new EventTypeJSONDeserializer();
         $this->addressDeserializer = new AddressJSONDeserializer();
         $this->calendarDeserializer = new CalendarDeserializer();
+        $this->themeDeserializer = new ThemeJSONDeserializer();
     }
 
     /**
@@ -48,6 +62,10 @@ class MajorInfoJSONDeserializer extends JSONDeserializer
         $data = parent::deserialize($data);
         $this->validator->validate($data);
 
+        $type = $this->typeDeserializer->deserialize(
+            new StringLiteral(json_encode($data['type']))
+        );
+
         $address = $this->addressDeserializer->deserialize(
             new StringLiteral(json_encode($data['address']))
         );
@@ -56,12 +74,14 @@ class MajorInfoJSONDeserializer extends JSONDeserializer
 
         $theme = null;
         if (!empty($data['theme'])) {
-            $theme = new Theme($data['theme']['id'], $data['theme']['label']);
+            $theme = $this->themeDeserializer->deserialize(
+                new StringLiteral(json_encode($data['theme']))
+            );
         }
 
         return new MajorInfo(
             new Title($data['name']['nl']),
-            new EventType($data['type']['id'], $data['type']['label']),
+            $type,
             $address,
             $calendar,
             $theme
