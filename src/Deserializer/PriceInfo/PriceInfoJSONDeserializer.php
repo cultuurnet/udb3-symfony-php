05fbd38dc72a6d9671c6c9c63d43388b29353525
Bert Ramakers <bertramakers@me.com>
2016-09-23 17:01:36 +0200

    III-1225: Updated dependencies and fixed PriceInfoJSONDeserializer.

--- src/Deserializer/PriceInfo/PriceInfoJSONDeserializer.php
+++ src/Deserializer/PriceInfo/PriceInfoJSONDeserializer.php
@@ -4,27 +4,28 @@ namespace CultuurNet\UDB3\Symfony\Deserializer\PriceInfo;
 
 use CultuurNet\Deserializer\JSONDeserializer;
 use CultuurNet\Deserializer\MissingValueException;
+use CultuurNet\UDB3\PriceInfo\BasePrice;
 use CultuurNet\UDB3\PriceInfo\Price;
-use CultuurNet\UDB3\PriceInfo\PriceCategory;
 use CultuurNet\UDB3\PriceInfo\PriceInfo;
-use CultuurNet\UDB3\PriceInfo\PriceInfoItem;
+use CultuurNet\UDB3\PriceInfo\Tariff;
 use ValueObjects\Money\Currency;
 use ValueObjects\String\String as StringLiteral;
 
-/**
- * @todo Move data validation to a separate class when III-1325 is merged.
- */
 class PriceInfoJSONDeserializer extends JSONDeserializer
 {
     /**
      * @param StringLiteral $data
      * @return PriceInfo
+     *
+     * @throws MissingValueException
+     * @throws \Exception
      */
     public function deserialize(StringLiteral $data)
     {
         $data = parent::deserialize($data);
 
-        $items = [];
+        // @todo Move data validation to a separate DataValidator class when III-1325 is merged.
+        $basePrices = 0;
 
         foreach ($data as $itemData) {
             if (!isset($itemData['category'])) {
@@ -32,7 +33,7 @@ class PriceInfoJSONDeserializer extends JSONDeserializer
             }
 
             if (!isset($itemData['name']) && $itemData['category'] !== 'base') {
-                throw new MissingValueException('The name property is required for each priceInfo item except base.');
+                throw new MissingValueException('The name property is required for each priceInfo item (except base).');
             }
 
             if (!isset($itemData['price'])) {
@@ -43,14 +44,43 @@ class PriceInfoJSONDeserializer extends JSONDeserializer
                 throw new MissingValueException('The priceCurrency property is required for each priceInfo item.');
             }
 
-            $items[] = new PriceInfoItem(
-                PriceCategory::fromNative($itemData['category']),
-                new StringLiteral($itemData['name']),
-                new Price($itemData['price']),
-                Currency::fromNative($itemData['priceCurrency'])
-            );
+            if ($itemData['category'] == 'base') {
+                $basePrices++;
+            }
         }
 
-        return new PriceInfo($items);
+        if ($basePrices > 1) {
+            throw new \Exception('priceInfo should not contain more than one item with the "base" category.');
+        }
+        if ($basePrices < 1) {
+            throw new \Exception('priceInfo should contain one item with the "base" category.');
+        }
+        // end @todo
+
+        $basePrice = null;
+        $tariffs = [];
+
+        foreach ($data as $itemData) {
+            if ($itemData['category'] == 'base') {
+                $basePrice = new BasePrice(
+                    new Price($itemData['price']),
+                    Currency::fromNative($itemData['priceCurrency'])
+                );
+            } else {
+                $tariffs[] = new Tariff(
+                    new StringLiteral($itemData['name']),
+                    new Price($itemData['price']),
+                    Currency::fromNative($itemData['priceCurrency'])
+                );
+            }
+        }
+
+        $priceInfo = new PriceInfo($basePrice);
+
+        foreach ($tariffs as $tariff) {
+            $priceInfo = $priceInfo->withExtraTariff($tariff);
+        }
+
+        return $priceInfo;
     }
 }
