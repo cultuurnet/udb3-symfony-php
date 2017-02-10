8b6ef10f41a115398418eb519a73c7970724e9d5
Luc Wollants <luc.wollants@apptite.be>
2017-02-10 13:44:17 +0100

    III-1802 Removed the obsolete PlaceLookupServiceInterface from the rest read place controller.

--- src/Place/ReadPlaceRestController.php
+++ src/Place/ReadPlaceRestController.php
@@ -2,13 +2,9 @@
 
 namespace CultuurNet\UDB3\Symfony\Place;
 
-use CultuurNet\Hydra\PagedCollection;
 use CultuurNet\UDB3\EntityServiceInterface;
-use CultuurNet\UDB3\Event\ReadModel\DocumentGoneException;
-use CultuurNet\UDB3\Place\ReadModel\Lookup\PlaceLookupServiceInterface;
 use CultuurNet\UDB3\Symfony\ApiProblemJsonResponseTrait;
 use CultuurNet\UDB3\Symfony\JsonLdResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 class ReadPlaceRestController
 {
@@ -23,20 +19,12 @@ class ReadPlaceRestController
     private $service;
 
     /**
-     * @var PlaceLookupServiceInterface
-     */
-    private $lookupService;
-
-    /**
      * @param EntityServiceInterface $service
-     * @param PlaceLookupServiceInterface $lookupService
      */
     public function __construct(
-        EntityServiceInterface $service,
-        PlaceLookupServiceInterface $lookupService
+        EntityServiceInterface $service
     ) {
         $this->service = $service;
-        $this->lookupService = $lookupService;
     }
 
     /**
@@ -59,37 +47,5 @@ class ReadPlaceRestController
         }
 
         return $response;
-    }
-
-    /**
-     * @param Request $request
-     * @return JsonLdResponse
-     */
-    public function getByPostalCode(Request $request)
-    {
-        // @todo Add & process pagination parameters
-        // @todo Validate zipcode
-        $zipCode = $request->query->get('zipcode');
-
-        $ids = $this->lookupService->findPlacesByPostalCode($zipCode);
-
-        $members = [];
-        if (!empty($ids)) {
-            $members = array_map(
-                function ($id) {
-                    return json_decode($this->service->getEntity($id));
-                },
-                $ids
-            );
-        }
-
-        $pagedCollection = new PagedCollection(
-            1,
-            1000,
-            $members,
-            count($members)
-        );
-
-        return new JsonLdResponse($pagedCollection);
     }
 }
