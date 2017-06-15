f967442b4bb60ec576a3f93d05d067f19ae41ef0
Bram Cordie <bramcordie@gmail.com>
2017-06-15 10:33:12 +0200

    III-2166: Require language when uploading an image

--- src/Media/EditMediaRestController.php
+++ src/Media/EditMediaRestController.php
@@ -2,6 +2,7 @@
 
 namespace CultuurNet\UDB3\Symfony\Media;
 
+use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\Media\ImageUploaderInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,6 +29,7 @@ class EditMediaRestController
 
         $description = $request->request->get('description');
         $copyrightHolder = $request->request->get('copyrightHolder');
+        $language = $request->request->get('language');
 
         if (!$description) {
             return new JsonResponse(['error' => "description required"], 400);
@@ -37,13 +39,18 @@ class EditMediaRestController
             return new JsonResponse(['error' => "copyright holder required"], 400);
         }
 
+        if (!$language) {
+            return new JsonResponse(['error' => "language required"], 400);
+        }
+
         $response = new JsonResponse();
         $file = $request->files->get('file');
 
         $commandId = $this->imageUploader->upload(
             $file,
             new StringLiteral($description),
-            new StringLiteral($copyrightHolder)
+            new StringLiteral($copyrightHolder),
+            new Language($language)
         );
 
         $response->setData(['commandId' => $commandId]);
