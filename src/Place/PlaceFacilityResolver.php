f66e6ae8b0dc8f2367234dd107680bd338cc96ed
Luc Wollants <luc.wollants@apptite.be>
2017-12-05 09:54:04 +0100

    III-2413 Create EventFacilityResolver for facilities on events.

--- src/Place/PlaceFacilityResolver.php
+++ src/Place/PlaceFacilityResolver.php
@@ -3,38 +3,19 @@
 namespace CultuurNet\UDB3\Symfony\Place;
 
 use CultuurNet\UDB3\Facility;
-use CultuurNet\UDB3\Symfony\Offer\OfferFacilityResolverInterface;
-use ValueObjects\StringLiteral\StringLiteral;
- 
-class PlaceFacilityResolver implements OfferFacilityResolverInterface
+use CultuurNet\UDB3\Symfony\Offer\OfferFacilityResolver;
+
+class PlaceFacilityResolver extends OfferFacilityResolver
 {
     /**
-     * @var Facility[]
+     * @inheritdoc
      */
-    private $facilities;
-
-    /**
-     * PlaceTypeResolver constructor.
-     */
-    public function __construct()
-    {
-        $this->facilities = [
+    final protected function getFacilities() {
+        return [
             "3.13.1.0.0" => new Facility("3.13.1.0.0","Voorzieningen voor assistentiehonden"),
             "3.23.3.0.0" => new Facility("3.23.3.0.0","Rolstoel ter beschikking"),
             "3.25.0.0.0" => new Facility("3.25.0.0.0","Contactpunt voor personen met handicap"),
             "3.26.0.0.0" => new Facility("3.26.0.0.0","Parkeerplaats"),
         ];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function byId(StringLiteral $facilityId)
-    {
-        if (!array_key_exists((string) $facilityId, $this->facilities)) {
-            throw new \Exception("Unknown place facility id '{$facilityId}'");
-        }
-
-        return $this->facilities[(string) $facilityId];
     }
 }
