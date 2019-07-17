9654caaf24d2a7243fa0f19b08191db045e4b741
Bert Ramakers <bert@madewithlove.be>
2019-07-17 15:20:33 +0200

    III-2961 Remove command id from label create endpoint

--- src/Label/EditRestController.php
+++ src/Label/EditRestController.php
@@ -8,6 +8,7 @@ use CultuurNet\UDB3\Label\ValueObjects\Privacy;
 use CultuurNet\UDB3\Label\ValueObjects\Visibility;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use ValueObjects\Identity\UUID;
 
 class EditRestController
@@ -34,21 +35,16 @@ class EditRestController
     {
         $bodyAsArray = json_decode($request->getContent(), true);
 
-        $writeResult = $this->writeService->create(
+        $uuid = $this->writeService->create(
             new LabelName($bodyAsArray['name']),
             Visibility::fromNative($bodyAsArray['visibility']),
             Privacy::fromNative($bodyAsArray['privacy'])
         );
 
-        return new JsonResponse($writeResult);
+        return new JsonResponse(['uuid' => $uuid->toNative()]);
     }
 
-    /**
-     * @param Request $request
-     * @param string $id
-     * @return JsonResponse
-     */
-    public function patch(Request $request, $id)
+    public function patch(Request $request, string $id): Response
     {
         $bodyAsArray = json_decode($request->getContent(), true);
         $commandType = CommandType::fromNative($bodyAsArray['command']);
@@ -57,19 +53,19 @@ class EditRestController
 
         switch ($commandType) {
             case CommandType::MAKE_VISIBLE():
-                $writeResult = $this->writeService->makeVisible($id);
+                $this->writeService->makeVisible($id);
                 break;
             case CommandType::MAKE_INVISIBLE():
-                $writeResult = $this->writeService->makeInvisible($id);
+                $this->writeService->makeInvisible($id);
                 break;
             case CommandType::MAKE_PUBLIC():
-                $writeResult = $this->writeService->makePublic($id);
+                $this->writeService->makePublic($id);
                 break;
             case CommandType::MAKE_PRIVATE():
-                $writeResult = $this->writeService->makePrivate($id);
+                $this->writeService->makePrivate($id);
                 break;
         }
 
-        return new JsonResponse($writeResult);
+        return new Response();
     }
 }
